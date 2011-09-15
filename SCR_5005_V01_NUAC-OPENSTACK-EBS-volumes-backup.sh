d040f2120c3f9bab61f7abeca3f6e2e8b23058f3
Razique <razique.mahroua@gmail.com>
2011-09-15 16:40:16 +0200

    - The script uses now "find" instead of the "$dateFileBefore"
    - The script uses now "find" instead of the "$dateFileBefore" in order
    to remove old dumps and old backup. We ensure that if the script fails
    for whatever reason, old files are removed.
    
    - Added a new setting "enable_checksum" which allows you to enable or
    disable a sha1 checksum on the file

--- SCR_5005_V01_NUAC-OPENSTACK-EBS-volumes-backup.sh
+++ SCR_5005_V01_NUAC-OPENSTACK-EBS-volumes-backup.sh
@@ -8,18 +8,30 @@
 # 		Revision note    
 # V00 : Initial version
 # V01 : The "create_tar" function has been modified, is uses now the second parameter in order to get the volume name
-  
+# V02 : In order to speed up the process, md5sum was replaced by sha1sum
+# V03 : Added a loop which ckecks that the backup volume is mounted (via sshfs) for every volume, otherwise the backup is aborted 
+# V04 : The script uses now "find" instead of the "$dateFileBefore" in order to remove old dumps and old backup. We ensure that if the script fails for whatever reason, old files are removed.
+
 #  		Notes    	
 # This script is meant to be launched from you cloud-manager server. It connects to all running instances, 
 # runs a mysqldump (Debian flavor), mounts the snapshoted LVM volumes and create a TAR on a destination directory. You can disable the mysqldumps if you don't use mysql/ or debian's instances.
 # The script can be croned everyday, a rotation makes sure that only a backup per day, for a week exists, while older ones are deleted. When the backup is over, an email is sent to you, with some details.
 
 # A test mode "lvm_limit_one" is available, it allows you to run the whole script for only one LVM volume.
+# There is also two interesting settings : "enable_checksum" which allows you to enable or disable a sha1 checksum on the file. Sometimes it could be important to disable it, since the required time for the checksum 
+# could ve very long depending on the file size
 	
 # 		Usage
-# chmod +x SCR_5005_$version_NUAC-EBS-volumes-backup.sh
-# ./SCR_5005_$version_NUAC-EBS-volumes-backup.sh           
+# chmod +x SCR_5005_$version_NUAC-OPENSTACK-EBS-volumes-backup.sh
+# ./SCR_5005_$version_NUAC-OPENSTACK-EBS-volumes-backup.sh           
 
+# Settings
+	lvm_test_mode=0
+	snapshot_max_size=50
+	backups_retention_days=3
+	enable_checksum=0
+	enable_mysql_dump=1
+	enable_mail_notification=1
 # Binaries
 	MOUNT=/bin/mount
 	UMOUNT=/bin/umount
@@ -32,7 +44,7 @@
 	MKDIR=/bin/mkdir
 	RM=/bin/rm
 	TAR=/bin/tar
-	MD5SUM=/usr/bin/md5sum
+	SHA1SUM=/usr/bin/sha1sum
 	GREP=/bin/grep
 	AWK=/usr/bin/awk
 	HEAD=/usr/bin/head
@@ -41,17 +53,15 @@
 	CAT=/bin/cat
 	DU=/usr/bin/du
 	SENDMAIL=/usr/sbin/sendmail
+	MOUNT=/bin/mount
+	FIND=/usr/bin/find
 # Misc
-	lvm_limit_one=0
-	snapshot_max_size=50
 	ssh_params="-o StrictHostKeyChecking=no -T -i /root/creds/nuage.pem"
 	ssh_known=/root/.ssh/known_hosts
 	ubuntu_ami="ami-0000000b"
 # Mail
-	enable_mail_notification=1
 	email_recipient="razique.mahroua@gmail.com"
 # MySQL
-	enable_mysql_dump=1
 	mysql_backup_name="mysql-dump"
 	mysql_server_dpkg_name="mysql-server"
 	mysql_backup_user="backup"
@@ -60,21 +70,24 @@
 	startTime=`date '+%s'`
 	dateMail=`date '+%d/%m at %H:%M:%S'`
 	dateFile=`date '+%d_%m_%Y'`
-	dateFileBefore=`date --date='1 week ago' '+%d_%m_%Y'`
 # Paths
 	email_tmp_file=/root/ebs_backup_status.tmp
-	#backup_destination=/root/BACKUP/EBS-VOL
-	backup_destination=/var/lib/glance/images/BACKUP/EBS-VOL
+	backup_destination=/root/BACKUP/EBS-VOL
 	mysql_backup_path=/home/mysql/backup
 	mount_point=/mnt
+	find_temp_file=/tmp/find_result.tmp
 # Messages
 	mailnotifications_disabled="The mail notifications are disabled"
 	mysqldump_disabled="The mysqldumps are disabled"
 	mysql_not_instaled="mysql is not installed, nothing to dump"
-	dir_exists="The directory already exists"
-	nothing="Nothing to remove"
+	dir_exists="The backup directory already exists"
+	old_dumps_not_found="Not any old backups to remove..."
+	old_dumps_found="Removing old backups..."
+	mount_ok="The backup volume is mounted. Proceed..."
+	mount_ko="The backup volume is not mounted. The backup has been aborted..."
 
 #### ---------------------------------------------- DO NOT EDIT AFTER THAT LINE ----------------------------------------------  #####
+# We create the temporary file which will be used for the mail notifications
 if [ ! -f $email_tmp_file ]; then
 	touch $email_tmp_file
 else
@@ -83,10 +96,9 @@ fi
 echo -e "Backup Start Time - $dateMail" >> $email_tmp_file
 
 # 1- Main functions
-
 ## Fetch volumes infos
 function get_lvs () {
-	if [ $lvm_limit_one -eq 0 ]; then
+	if [ $lvm_test_mode -eq 0 ]; then
 		$LVDISPLAY | $GREP "LV Name" | $AWK '{ print $3 }' 
 	else	
 		$LVDISPLAY | $GREP "LV Name" | $AWK '{ print $3 }' | $HEAD -1
@@ -117,9 +129,8 @@ function mysql_backup () {
 
 function ssh_connect () {
 	$CAT /dev/null > $ssh_known 
-	
 	$SSH $ssh_params $1@$ip_address <<-EOF
-		if [ `dpkg -l | $GREP $mysql_server_dpkg_name | $WC -l` -eq 0 ]; then
+		if [ \`dpkg -l | $GREP $mysql_server_dpkg_name | $WC -l\` -eq 0 ]; then
 			echo $mysql_not_instaled;
 		else
 			# Dump directory creation
@@ -133,13 +144,14 @@ function ssh_connect () {
 			$MYSQLDUMP --all-databases -u $mysql_backup_user -p$mysql_backup_pass > $mysql_backup_path/$mysql_backup_name-$dateFile.sql;
 
 			# Old dumps deletion
-			if [ -f $mysql_backup_path/$mysql_backup_name-$dateFileBefore ]; then
-				rm $mysql_backup_path/$mysql_backup_name-$dateFileBefore.sql
+			$FIND $mysql_backup_path -type f -name "$mysql_backup_name*" -mtime +$backups_retention_days | wc -l > $find_temp_file
+			if [ \`cat $find_temp_file\` -ge 1 ]; then
+				echo $old_dumps_found
+				$FIND $mysql_backup_path -type f -name "$mysql_backup_name*" -mtime +$backups_retention_days -exec rm -f {} \;
 			else
-				echo $nothing;
+				echo $old_dumps_not_found
 			fi
 		fi
-		exit
 	EOF
 }
 
@@ -164,13 +176,15 @@ function create_tar () {
 		$MKDIR $backup_destination/$2;
 	fi
 		cd $backup_destination/$2
-		$TAR --exclude={"lost+found","mysql/data","mysql/tmp"} -czf $2_$dateFile.tar.gz -C $mount_point . 
-		$MD5SUM $backup_destination/$2/$2_$dateFile.tar.gz > $backup_destination/$2/$2_$dateFile.checksum
+		#$TAR --exclude={"lost+found","mysql/data","mysql/tmp"} -czf $2_$dateFile.tar.gz -C $mount_point . 
+		
+		if [ $enable_checksum -eq 1 ]; then
+			$SHA1SUM $backup_destination/$2/$2_$dateFile.tar.gz > $backup_destination/$2/$2_$dateFile.checksum
+		fi
 	
-	if [ -f $backup_destination/$2_$dateFileBefore.tar ]; then
-		# Rotation des anciens fichiers 
-		$RM $backup_destination/$2_$dateFileBefore.tar;
-		$RM $backup_destination/$2_$dateFileBefore.checksum;
+	if [ `$FIND $backup_destination -type f -name "$2*" -mtime +$backups_retention_days | wc -l` -ge 1 ]; then
+		# Old files deletion
+		$FIND $backup_destination -type f -name "$2*" -mtime +$backups_retention_days -exec rm -f {} \;
 	else 
 		echo $nothing;
 	fi
@@ -178,7 +192,7 @@ function create_tar () {
 
 # 4- Databases backup
 if [ $enable_mysql_dump -eq 0 ]; then
-	echo $mysqldump_disabled
+	echo $mysqldump_disabled >> $email_tmp_file
 else
 	mysql_backup
 fi
@@ -188,37 +202,55 @@ for i in `get_lvs`; do
 	startTimeLVM=`date '+%s'`
 	
 	echo -e "\n ######################### `get_lvs_name $i` #########################"
-   
-   	# Volumes retrieval
-   	echo -e "\n STEP 1 :Snapshot creation"
-   	create_snapshot `get_lvs_name $i` $i $snapshot_max_size 
+ 
+ 	# We ensure that a backup disk is mounted before proceeding
+	if [ `$MOUNT | $GREP "$backup_destination" | $WC -l` -eq 1 ]; then
+		echo -e $mount_ok >> $email_tmp_file
+	else
+		echo $mount_ko >> $email_tmp_file
 	
-   	echo -e "\n STEP 2 : Table partition creation"
-   	$KPARTX -av $i-SNAPSHOT
+		if [ $enable_mail_notification -eq 0 ]; then
+			echo $mailnotifications_disabled
+		else
+			echo -e "---------------------------------------" >> $email_tmp_file
+			echo -e "To : $recipient \nSubject : The EBS volumes backup has been aborted the $dateMail ! \n`$CAT $email_tmp_file`" | $SENDMAIL $email_recipient
+			rm $email_tmp_file
+		fi
+		exit
+	fi
+
+  	# Volumes retrieval
+  	echo -e "\n STEP 1 :Snapshot creation"
+  	create_snapshot `get_lvs_name $i` $i $snapshot_max_size 
 	
-   	echo -e "\n STEP 3 : Volumes mounting"
-   	sleep 1;
+  	echo -e "\n STEP 2 : Table partition creation"
+  	sleep 1;
+  	$KPARTX -av $i-SNAPSHOT
+	
+  	echo -e "\n STEP 3 : Volumes mounting"
+  	sleep 1;
 	$MOUNT "/dev/mapper/nova--volumes-volume--`get_lvs_id $i`--SNAPSHOT1" $mount_point
 	
-   	echo -e "\n STEP 4 : Archive creation"
-   	create_tar $i `get_lvs_name $i`
-   
-   	echo -e "\n STEP 5 : Umount volume"
-   	$UMOUNT $mount_point
-   
-   	echo -e "\n STEP 6 : Table partition remove"
-   	$KPARTX -d $i-SNAPSHOT
-   
-   	echo -e "\n STEP 7 : Snapshot deletion "
+  	echo -e "\n STEP 4 : Archive creation"
+ 	create_tar $i `get_lvs_name $i`
+  
+  	echo -e "\n STEP 5 : Umount volume"
+  	$UMOUNT $mount_point
+  
+  	echo -e "\n STEP 6 : Table partition remove"
+  	sleep 1;
+  	$KPARTX -d $i-SNAPSHOT
+  
+  	echo -e "\n STEP 7 : Snapshot deletion "
 	sleep 1;
-   	$LVREMOVE -f $i-SNAPSHOT
+  	$LVREMOVE -f $i-SNAPSHOT
 
 	#Time accounting per volume
 	time_accounting `date '+%s'` $startTimeLVM
 	
 	# Mail notification creation
 	backup_size=`$DU -h $backup_destination/\`get_lvs_name $i\` | $CUT -f 1`
-	echo -e "$i - $hours h $minutes m and $seconds seconds. Size - $backup_size" >> $email_tmp_file	
+	echo -e "$backup_destination/`get_lvs_name $i` - $hours h $minutes m and $seconds seconds. Size - $backup_size" >> $email_tmp_file	
 done
 
 # 6- Mail notification
